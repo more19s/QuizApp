83d3ebbdb2e797c75ab3e827464750276b2321b0
Swapnil Sudhir More <Smore6602@conestogac.on.ca>
2024-07-15 12:52:26 -0400

    Intro login and signup activities created.Connected the firebase DB to create users.

---
+++ app/src/main/java/com/example/quizapp/LoginActivity.kt
@@ -0,0 +1,49 @@
+package com.example.quizapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.quizapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityLoginBinding
+    lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth =FirebaseAuth.getInstance()
+
+        binding.btnLogin.setOnClickListener{
+            login()
+        }
+
+    }
+
+    private  fun login(){
+        val email = binding.etEmailAddress.text.toString()
+        val password = binding.etPassword.text.toString()
+        
+        if (email.isBlank()||password.isBlank()){
+            Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show()
+        }
+        
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                    Toast.makeText(this, "Authentication failed", Toast.LENGTH_SHORT).show()
+            }
+            }
+
+    }
+}
---
+++ app/src/main/java/com/example/quizapp/SignupActivity.kt
@@ -0,0 +1,54 @@
+package com.example.quizapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.quizapp.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignupActivity : AppCompatActivity() {
+    lateinit var firebaseAuth: FirebaseAuth
+
+    lateinit var binding: ActivitySignupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.btnSignup.setOnClickListener{
+            signUpUser()
+        }
+
+    }
+
+    private fun  signUpUser(){
+        val  email = binding.etEmailAddress.text.toString()
+        val password = binding.etPassword.text.toString()
+        val cPassword = binding.etCPassword.text.toString()
+
+        if (email.isBlank()||password.isBlank()||cPassword.isBlank()){
+            Toast.makeText(this, "Fields cannot be blank.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (password!=cPassword){
+            Toast.makeText(this, "Password and confirm password must match.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, "Error creating user", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+}
