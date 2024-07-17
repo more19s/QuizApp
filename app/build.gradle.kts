82d3d1ee1e786773ee8b35be07223c9a1ed8540c
Swapnil Sudhir More <Smore6602@conestogac.on.ca>
2024-07-17 12:07:41 -0400

    Created a material navigation drawer in main activity. Navigation menu created for adding items in navigation drawer.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.androidx.navigation.fragment.ktx)
+    implementation ("com.google.android.material:material:1.5.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
