5cc821b88418e30476219a5bd3e51b5bf0ef250b
Swapnil Sudhir More <Smore6602@conestogac.on.ca>
2024-07-19 12:45:48 -0400

    added clickable functionality to the navigation drawer icon

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.quizapp
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quizapp.databinding.ActivityLoginBinding
+import com.example.quizapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupViews()
+
+    }
+    fun setupViews(){
+        setupDrawerLayout()
+    }
+
+    fun setupDrawerLayout(){
+        setSupportActionBar(binding.appBar)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this,binding.mainDrawer,R.string.app_name,R.string.app_name)
+        actionBarDrawerToggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true
         }
+        return super.onOptionsItemSelected(item)
     }
 }
